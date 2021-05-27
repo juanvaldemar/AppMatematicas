ec2f1b5850e06b26b55142855ba6e03dc5d54cc0
DonValdemar <juan.colichon@vasslatam.com>
2021-05-27 08:55:46 -0500

    agregando contar colores

---
+++ app/src/main/java/com/daniel/appmatematicas/view/fragmentos/ColorFragment.java
@@ -0,0 +1,27 @@
+package com.daniel.appmatematicas.view.fragmentos;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.daniel.appmatematicas.R;
+
+
+public class ColorFragment extends Fragment {
+
+
+    public ColorFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.numeros_colores, container, false);
+    }
+}
---
+++ app/src/main/java/com/daniel/appmatematicas/view/fragmentos/SumarFragment.java
@@ -0,0 +1,25 @@
+package com.daniel.appmatematicas.view.fragmentos;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.daniel.appmatematicas.R;
+
+public class SumarFragment extends Fragment {
+
+    public SumarFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.suma, container, false);
+    }
+}
