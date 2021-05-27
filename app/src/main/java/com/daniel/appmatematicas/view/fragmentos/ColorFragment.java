48ddea3dc92de88d292ff25d34e2c6ac956c5999
DonValdemar <juan.colichon@vasslatam.com>
2021-05-27 16:42:45 -0500

    contar_colores

--- app/src/main/java/com/daniel/appmatematicas/view/fragmentos/ColorFragment.java
+++ app/src/main/java/com/daniel/appmatematicas/view/fragmentos/ColorFragment.java
@@ -2,17 +2,45 @@ package com.daniel.appmatematicas.view.fragmentos;
 
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.daniel.appmatematicas.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 
 public class ColorFragment extends Fragment {
 
+    private int numeroAleatorioPrincipal;
+    private int valorSeleccionado;
+    private boolean seleccion;
+
+    private RelativeLayout mPrimeroR;
+    private RelativeLayout mSegundoR;
+    private RelativeLayout mTerceroR;
+    private RelativeLayout mCuartoR;
+    private RelativeLayout mQuintoR;
+    private RelativeLayout mSextoR;
+
+    private TextView mPrimero;
+    private TextView mSegundo;
+    private TextView mTercero;
+    private TextView mCuarto;
+    private TextView mQuinto;
+    private TextView mSexto;
+    private EditText contador;
 
     public ColorFragment() {
         // Required empty public constructor
@@ -22,6 +50,180 @@ public class ColorFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.numeros_colores, container, false);
+        View root = inflater.inflate(R.layout.numeros_colores, container, false);
+           initGenerados(root);
+        return root;
     }
+
+
+    private void initGenerados(View root) {
+        Random rd = new Random();
+        List<Integer> generados = new ArrayList<>();
+        for (int i = 1; i <= 6; i++) {
+            int aleatorio = 0;
+            boolean generado = false;
+            while (!generado) {
+                int posible = rd.nextInt(100);
+                if (!generados.contains(posible)) {
+                    generados.add(posible);
+                    aleatorio = posible;
+                    generado = true;
+                }
+            }
+            if (i == 6) {
+                initFonts(generados, 15,root);
+            }
+            System.out.println("Aleatorio: " + aleatorio);
+        }
+        initSeleccionEmpty(root);
+
+        initClicks(root);
+
+
+    }
+    private void initFonts(List<Integer> generados, int principal, View root) {
+
+
+        numeroAleatorioPrincipal = principal;
+
+        mPrimero = root.findViewById(R.id.primero);
+        mSegundo = root.findViewById(R.id.segundo);
+        mTercero = root.findViewById(R.id.tercero);
+        mCuarto = root.findViewById(R.id.cuarto);
+        mQuinto = root.findViewById(R.id.quinto);
+
+
+
+        mPrimero.setText(generados.get(0).toString());
+        mSegundo.setText("15");
+        mTercero.setText(generados.get(2).toString());
+        mCuarto.setText(generados.get(3).toString());
+        mQuinto.setText(generados.get(4).toString());
+
+        //encuentra_numero_text = findViewById(R.id.encuentra_numero_text);
+        //encuentra_numero_text.setText("Encuentra el número "+ this.numeroAleatorioPrincipal +":");
+    }
+
+
+    private void initSeleccionEmpty(View root) {
+
+        mPrimeroR = root.findViewById(R.id.seleccion_primero);
+        mPrimeroR.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.white));
+
+        mSegundoR= root.findViewById(R.id.seleccion_segundo);
+        mSegundoR.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.white));
+
+        mTerceroR= root.findViewById(R.id.seleccion_tercero);
+        mTerceroR.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.white));
+
+        mCuartoR = root.findViewById(R.id.seleccion_cuarto);
+
+        mCuartoR.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.white));
+
+        mQuintoR = root.findViewById(R.id.seleccion_quinto);
+
+        mQuintoR.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.white));
+
+
+
+    }
+
+    private void initClicks(View root) {
+
+        mPrimeroR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initSeleccionEmpty(root);
+                valorSeleccionado = Integer.parseInt(mPrimero.getText().toString());
+                seleccion = true;
+                mPrimeroR.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.gray));
+
+            }
+        });
+        mSegundoR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initSeleccionEmpty(root);
+                valorSeleccionado = Integer.parseInt(mSegundo.getText().toString());
+                seleccion = true;
+                mSegundoR.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.gray));
+
+            }
+        });
+        mTerceroR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initSeleccionEmpty(root);
+                valorSeleccionado = Integer.parseInt(mTercero.getText().toString());
+                seleccion = true;
+                mTerceroR.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.gray));
+
+            }
+        });
+        mCuartoR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initSeleccionEmpty(root);
+                valorSeleccionado = Integer.parseInt(mCuarto.getText().toString());
+                seleccion = true;
+                mCuartoR.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.gray));
+
+            }
+        });
+        mQuintoR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initSeleccionEmpty(root);
+                valorSeleccionado = Integer.parseInt(mQuinto.getText().toString());
+                seleccion = true;
+                mQuintoR.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.gray));
+
+            }
+        });
+
+        Button validar = root.findViewById(R.id.validar);
+        validar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!seleccion){
+                    //Toast.makeText(BuscarNumeroActivity.this,"Por favor seleccione una opcción.",Toast.LENGTH_LONG).show();
+                    showSnackBar("¡Por favor seleccione una opcción valida!");
+                }else{
+                    if(valorSeleccionado == numeroAleatorioPrincipal){
+                        //Toast.makeText(BuscarNumeroActivity.this,"Seleccionó "+valorSeleccionado,Toast.LENGTH_SHORT).show();
+                        showSnackBar("¡Muy bien!");
+                        //startActivity(new Intent(getActivity(), PerfilActivity.class));
+                        // listaCalificacion.add(true);
+                    }else{
+                        //Toast.makeText(BuscarNumeroActivity.this,"Incorrecto "+valorSeleccionado,Toast.LENGTH_SHORT).show();
+                        showSnackBar("¡Oh no fallaste!");
+                        //listaCalificacion.add(false);
+                        // startActivity(new Intent(getActivity(), PerfilActivity.class));
+
+                    }
+
+                   /* contador = findViewById(R.id.contador);
+                    int contador_ = Integer.parseInt(contador.getText().toString());
+
+                    iniciarNuevamente();
+                    int contador2 = contador_+1;
+                    if(contador2 > 9){
+                        prefs.edit().putString("prefs_puntaje", listaCalificacion.toString()).commit();
+                        showSnackBar(listaCalificacion.toString());
+                        startActivity(new Intent(BuscarNumeroActivity.this, ResultadoFinal.class));
+                        finish();
+                    }
+                    contador.setText(contador2+ "");
+
+                    */
+                }
+            }
+        });
+
+    }
+
+    public void showSnackBar(String msg) {
+        Toast.makeText(getActivity(),""+msg,Toast.LENGTH_SHORT).show();
+    }
+
 }
