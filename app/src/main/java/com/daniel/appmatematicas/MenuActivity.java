b1604a1fd038475e32381682fb0053eecc362111
DonValdemar <juan.colichon@vasslatam.com>
2021-05-27 17:47:35 -0500

    agregando lapiz

--- app/src/main/java/com/daniel/appmatematicas/MenuActivity.java
+++ app/src/main/java/com/daniel/appmatematicas/MenuActivity.java
@@ -58,7 +58,8 @@ public class MenuActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_resta, R.id.nav_contar_color)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_resta, R.id.nav_contar_color,
+                R.id.nav_contar_color, R.id.nav_enlatados_colores, R.id.LongitudLapizFragment)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
