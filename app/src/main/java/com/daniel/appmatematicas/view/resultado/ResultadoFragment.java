5a41d28f5c852d17962c1e9b33bfde244a2d5c50
DonValdemar <juan.colichon@vasslatam.com>
2021-06-23 15:57:37 -0500

    x

--- app/src/main/java/com/daniel/appmatematicas/view/resultado/ResultadoFragment.java
+++ app/src/main/java/com/daniel/appmatematicas/view/resultado/ResultadoFragment.java
@@ -3,10 +3,12 @@ package com.daniel.appmatematicas.view.resultado;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.daniel.appmatematicas.R;
 
@@ -24,7 +26,14 @@ public class ResultadoFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_resultado, container, false);
 
 
-
+        Button btnCerrar;
+        btnCerrar = root.findViewById(R.id.cerrar);
+        btnCerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(getActivity(), R.id.nav_host_fragment).navigate(R.id.nav_home);
+            }
+        });
         return root;
     }
 }
